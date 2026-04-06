a6e45f45a326ccc3614c58d53ea0531f58d60537
pofazze <pofazze@gmail.com>
2026-04-06 18:44:29 +0000

    feat: add approvals controller

---
+++ server/src/modules/approvals/approvals.controller.js
@@ -0,0 +1,102 @@
+const service = require('./approvals.service');
+const { smApproveSchema, sendToClientSchema, clientRespondSchema } = require('./approvals.validation');
+const logger = require('../../utils/logger');
+
+class ApprovalsController {
+  async listSmPending(req, res, next) {
+    try {
+      const deliveries = await service.listSmPending(req.user.id);
+      res.json(deliveries);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async listByClient(req, res, next) {
+    try {
+      const deliveries = await service.listByClient(req.params.clientId);
+      res.json(deliveries);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async smApprove(req, res, next) {
+    try {
+      const { error, value } = smApproveSchema.validate(req.body);
+      if (error) return res.status(400).json({ error: error.details[0].message });
+
+      const result = await service.smApprove(value.delivery_id, value, req.user.id);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async sendToClient(req, res, next) {
+    try {
+      const { error, value } = sendToClientSchema.validate(req.body);
+      if (error) return res.status(400).json({ error: error.details[0].message });
+
+      const result = await service.sendToClient(value.client_id, value.items, req.user.id);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async listBatches(req, res, next) {
+    try {
+      const batches = await service.listBatches(req.params.clientId);
+      res.json(batches);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async revokeBatch(req, res, next) {
+    try {
+      const result = await service.revokeBatch(req.params.batchId, req.user.id);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async listWhatsAppGroups(req, res, next) {
+    try {
+      const groups = await service.listWhatsAppGroups();
+      res.json(groups);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getPublicBatch(req, res, next) {
+    try {
+      const data = await service.getBatchByToken(req.params.token);
+      res.json(data);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async clientRespond(req, res, next) {
+    try {
+      const { error, value } = clientRespondSchema.validate(req.body);
+      if (error) return res.status(400).json({ error: error.details[0].message });
+
+      const result = await service.clientRespond(
+        req.params.token,
+        req.params.itemId,
+        value.status,
+        value.rejection_reason,
+      );
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = new ApprovalsController();
